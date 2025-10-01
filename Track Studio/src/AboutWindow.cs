ac4d4d32b9d47f560ca3ef79ca44c326e92ec879
Owen Smart <Owen32Smart@gmail.com>
2025-09-30 22:02:52 -0400

    update dev branch to say version 0.2.0

--- Track Studio/src/AboutWindow.cs
+++ Track Studio/src/AboutWindow.cs
@@ -16,7 +16,7 @@ namespace MapStudio
 
         public override ImGuiWindowFlags Flags => ImGuiWindowFlags.NoDocking;
 
-        public static string AppVersion = "0.1.2";
+        public static string AppVersion = "0.2.0";
         string[] ChangeLog;
         string[] ChangeType;
 
